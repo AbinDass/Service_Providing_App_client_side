d59fb80b4a2fef1b2d5c6b32567e94ea45065b3c
AbinDass <abindas350@gmail.com>
2023-04-27 21:18:41 +0800

    render url added

--- src/API/axiosinstance.js
+++ src/API/axiosinstance.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
 const Axiosuser = Axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://social-expo.onrender.com",
 });
 
 const Axiosadmin = Axios.create({
-    baseURL: "http://localhost:8000/admin",
+    baseURL: "https://social-expo.onrender.com/admin",
 });
 
 export { Axiosuser, Axiosadmin };
