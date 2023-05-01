c0aff5dcf3e4c60d2a969b83131670c42caf43de
AbinDass <abindas350@gmail.com>
2023-05-01 15:49:19 +0800

    signup validation testing

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -7,6 +7,7 @@ import { LoginSocialGoogle } from "reactjs-social-login";
 import { googleAuth } from "../API/userAuth";
 import LocationSearch from "../components/LocationSearch";
 import Otp from "../components/Otp";
+import { FormValidate } from "../utilities/signupValidate";
 
 const initialstate = {
     firstname: "",
@@ -22,9 +23,15 @@ const SignUp = () => {
 
     const [formData, setFormData] = useState(initialstate);
     const [otpup, setOtpUp] = useState(false);
+    const [errors, setErrors] = useState({});
 
-
+    const validateForm = () => {
+        const newErrors = FormValidate(formData)
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
     const hanadleChange = (e) => {
+        if (!validateForm()) return;
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
@@ -52,7 +59,7 @@ const SignUp = () => {
     };
     return (
         <div className="flex bg-black w-full">
-            <div className="text-white h-screen w-[50%] hidden md:flex md:flex-col items-center justify-center ">
+            <div className="text-white h-screen w-[50%] hidden md:flex md:flex-col items-center justify-center  ">
                 <img src="https://www.pngall.com/wp-content/uploads/5/Employment-PNG-Free-Image.png" alt="/" />
                 <h1 className=" text-4xl font-bold text-[#00df9a]">SOCIAL-EXPO</h1>
             </div>
@@ -70,40 +77,44 @@ const SignUp = () => {
                             <label htmlFor="firstname">first name</label>
                             <input
                                 type="text"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.firstname ? "border-red-500" : ""}`}
                                 id="firstname"
                                 name="firstname"
                                 value={formData.firstname}
                                 onChange={hanadleChange}
                                 placeholder="Your firstname"
                             />
+                            {errors.firstname && (<p className="text-red-500 mt-1 text-xs italic"> {errors.firstname}</p>)}
                         </div>
                         <div>
                             <label htmlFor="secondname">second name</label>
                             <input
                                 type="text"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.secondname ? "border-red-500" : ""}`}
                                 id="secondname"
                                 name="secondname"
                                 value={formData.secondname}
                                 onChange={hanadleChange}
                                 placeholder="Your secondname"
                             />
+                            {errors.secondname && (<p className="text-red-500 mt-1 text-xs italic"> {errors.secondname}</p>)}
                         </div>
                         <div>
                             <label htmlFor="email">Email</label>
                             <input
                                 type="email"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.email ? "border-red-500" : ""}`}
                                 id="email"
                                 name="email"
                                 value={formData.email}
                                 onChange={hanadleChange}
                                 placeholder="Your Email"
                             />
+                            {errors.email && (<p className="text-red-500 mt-1 text-xs italic"> {errors.email}</p>)}
                         </div>
-                        <div className="pt-5 px-5">
+                        <div className={`pt-5 px-5 ${errors?.location ? "border-red-500" : ""}`}>
                         <LocationSearch formLocation={updateLocation} />
+                        {errors.location && (<p className="text-red-500 mt-1 text-xs italic"> {errors.location}</p>)}
                     </div>
                         </div>
 
@@ -113,38 +124,41 @@ const SignUp = () => {
                             <label htmlFor="phoe">phone</label>
                             <input
                                 type="number"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.phone ? "border-red-500" : ""}`}
                                 id="phone"
                                 name="phone"
                                 value={formData.phone}
                                 onChange={hanadleChange}
                                 placeholder="Your phone"
                             />
+                            {errors.phone && (<p className="text-red-500 mt-1 text-xs italic"> {errors.phone}</p>)}
                         </div>
 
                         <div>
                             <label htmlFor="password">Password</label>
                             <input
                                 type="password"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.password ? "border-red-500" : ""}`}
                                 id="password"
                                 name="password"
                                 value={formData.password}
                                 onChange={hanadleChange}
                                 placeholder="Your Password"
                             />
+                            {errors.password && (<p className="text-red-500 mt-1 text-xs italic"> {errors.password}</p>)}
                         </div>
                         <div>
                             <label htmlFor="confirmpassword">confirm Password</label>
                             <input
                                 type="password"
-                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4 ${errors?.confirmpassword ? "border-red-500" : ""}`}
                                 id="confirmpassword"
                                 name="confirmpassword"
                                 value={formData.confirmpassword}
                                 onChange={hanadleChange}
                                 placeholder="Your Password"
                             />
+                            {errors.confirmpassword && (<p className="text-red-500 mt-1 text-xs italic"> {errors.confirmpassword}</p>)}
                         </div>
                         </div>
                     </form>
---
+++ src/utilities/signupValidate.js
@@ -0,0 +1,16 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-expressions */
+export const FormValidate = (formData) => {
+    const newErrors = {};
+    const keys = Object.keys(formData)
+    keys.map(key => {
+        if (!formData[key]) newErrors[key] = `${key} is required`;
+        else if (key === 'email') {
+            !/\S+@\S+\.\S+/.test(formData[key]) ? newErrors[key] = 'Email is invalid' : ''
+        }
+        else if (key === 'mobile') {
+            !/^\d{10}$/.test(formData.mobile) ? newErrors[key] = 'Mobile Number is invalid' : ''
+        }
+    })
+    return newErrors;
+}
